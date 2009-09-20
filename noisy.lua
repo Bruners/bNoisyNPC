ec25fa31bf5cb8963fa22146e78971bef3828323
Bruners <bruners@gmail.com>
2009-09-20 18:35:53 +0200

    initial commit

---
+++ noisy.lua
@@ -0,0 +1,25 @@
+﻿local noisyNPC = {
+	"Plugger Spazzring",
+	"Guzzling Patron",
+	"Coren Direbrew",
+	"Jaeren Sunsworn",
+}
+
+ChatFrame_AddMessageEventFilter("CHAT_MSG_MONSTER_SAY", function(self, event, msg, author)
+		for i=1, #noisyNPC do
+			return author == noisyNPC[i]
+		end
+end)
+
+ChatFrame_AddMessageEventFilter("CHAT_MSG_MONSTER_YELL", function(self, event, msg, author)
+		for i=1, #noisyNPC do
+			return author == noisyNPC[i]
+		end
+end)
+
+ChatFrame_AddMessageEventFilter("CHAT_MSG_MONSTER_EMOTE", function(self, event, msg, author)
+		for i=1, #noisyNPC do
+			return author == noisyNPC[i]
+		end
+end)
+
