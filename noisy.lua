d9811e52268f857b08b850702472c16eb253677c
Bruners <bruners@gmail.com>
2009-09-20 20:13:42 +0200

    add some more npcs

--- noisy.lua
+++ noisy.lua
@@ -3,23 +3,35 @@
 	"Guzzling Patron",
 	"Coren Direbrew",
 	"Jaeren Sunsworn",
+	"Gordok Brew Barke",
+	"Dark Iron Herald",
+	"Blix Fixwidget",
+	"Drohn's Distillery Barker",
+	"Greeb Ramrocket",
+	"Gordok Brew Barker",
 }
 
 ChatFrame_AddMessageEventFilter("CHAT_MSG_MONSTER_SAY", function(self, event, msg, author)
 		for i=1, #noisyNPC do
-			return author == noisyNPC[i]
+			if author == noisyNPC[i] then
+				return msg
+			end
 		end
 end)
 
 ChatFrame_AddMessageEventFilter("CHAT_MSG_MONSTER_YELL", function(self, event, msg, author)
 		for i=1, #noisyNPC do
-			return author == noisyNPC[i]
+			if author == noisyNPC[i] then
+				return msg
+			end
 		end
 end)
 
 ChatFrame_AddMessageEventFilter("CHAT_MSG_MONSTER_EMOTE", function(self, event, msg, author)
 		for i=1, #noisyNPC do
-			return author == noisyNPC[i]
+			if author == noisyNPC[i] then
+				return msg
+			end
 		end
 end)
 
